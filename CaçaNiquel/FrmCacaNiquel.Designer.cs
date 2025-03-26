65e0f70438ec4e109c6589087b8c12a3c9b9a6a8
ra2457102 <ra2457102@LDI319>
2025-03-26 20:02:07 -0300

    Add project files.

---
+++ CaçaNiquel/FrmCacaNiquel.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CaçaNiquel
+{
+    partial class frmCacaNiquel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblNiquel1 = new System.Windows.Forms.Label();
+            this.lblNiquel2 = new System.Windows.Forms.Label();
+            this.lblNiquel3 = new System.Windows.Forms.Label();
+            this.btnGirar = new System.Windows.Forms.Button();
+            this.tmrSorteioGeral = new System.Windows.Forms.Timer(this.components);
+            this.btnJogar = new System.Windows.Forms.Button();
+            this.tmrNiquel = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblNiquel1
+            // 
+            this.lblNiquel1.BackColor = System.Drawing.Color.Red;
+            this.lblNiquel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNiquel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNiquel1.ForeColor = System.Drawing.Color.White;
+            this.lblNiquel1.Location = new System.Drawing.Point(58, 138);
+            this.lblNiquel1.Name = "lblNiquel1";
+            this.lblNiquel1.Size = new System.Drawing.Size(57, 65);
+            this.lblNiquel1.TabIndex = 0;
+            this.lblNiquel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNiquel2
+            // 
+            this.lblNiquel2.BackColor = System.Drawing.Color.Red;
+            this.lblNiquel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNiquel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNiquel2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNiquel2.Location = new System.Drawing.Point(173, 138);
+            this.lblNiquel2.Name = "lblNiquel2";
+            this.lblNiquel2.Size = new System.Drawing.Size(57, 65);
+            this.lblNiquel2.TabIndex = 1;
+            this.lblNiquel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNiquel3
+            // 
+            this.lblNiquel3.BackColor = System.Drawing.Color.Red;
+            this.lblNiquel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNiquel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNiquel3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNiquel3.Location = new System.Drawing.Point(281, 138);
+            this.lblNiquel3.Name = "lblNiquel3";
+            this.lblNiquel3.Size = new System.Drawing.Size(57, 65);
+            this.lblNiquel3.TabIndex = 2;
+            this.lblNiquel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGirar
+            // 
+            this.btnGirar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGirar.Location = new System.Drawing.Point(5, 441);
+            this.btnGirar.Name = "btnGirar";
+            this.btnGirar.Size = new System.Drawing.Size(110, 37);
+            this.btnGirar.TabIndex = 3;
+            this.btnGirar.Text = "&Girar";
+            this.btnGirar.UseVisualStyleBackColor = true;
+            this.btnGirar.Click += new System.EventHandler(this.btnGirar_Click);
+            // 
+            // tmrSorteioGeral
+            // 
+            this.tmrSorteioGeral.Interval = 1000;
+            this.tmrSorteioGeral.Tick += new System.EventHandler(this.tmrSorteioGeral_Tick);
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnJogar.Location = new System.Drawing.Point(163, 301);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(86, 58);
+            this.btnJogar.TabIndex = 4;
+            this.btnJogar.Text = "Jogar";
+            this.btnJogar.UseVisualStyleBackColor = false;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
+            // 
+            // tmrNiquel
+            // 
+            this.tmrNiquel.Interval = 300;
+            this.tmrNiquel.Tick += new System.EventHandler(this.tmrNiquel_Tick);
+            // 
+            // frmCacaNiquel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 535);
+            this.Controls.Add(this.btnJogar);
+            this.Controls.Add(this.btnGirar);
+            this.Controls.Add(this.lblNiquel3);
+            this.Controls.Add(this.lblNiquel2);
+            this.Controls.Add(this.lblNiquel1);
+            this.Name = "frmCacaNiquel";
+            this.Text = "Caça Níquel 1.0.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNiquel1;
+        private System.Windows.Forms.Label lblNiquel2;
+        private System.Windows.Forms.Label lblNiquel3;
+        private System.Windows.Forms.Button btnGirar;
+        private System.Windows.Forms.Timer tmrSorteioGeral;
+        private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.Timer tmrNiquel;
+    }
+}
+
---
+++ CaçaNiquel/FrmCacaNiquel.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaçaNiquel
+{
+    public partial class frmCacaNiquel : Form
+    {
+        private Random sorteio = new Random();
+        private int niquel1, niquel2, niquel3;
+        private int contaGiro = 0;
+        private int contaNiquel = 1;
+        public frmCacaNiquel()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrSorteioGeral_Tick(object sender, EventArgs e)
+        {
+            MostraNiquel();
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            btnJogar.Text = "&STOP";
+            //contaGiro = 0;
+            tmrNiquel.Enabled = true;
+
+        }
+
+        private void tmrNiquel_Tick(object sender, EventArgs e)
+        {
+            if (contaNiquel == 1)
+            {
+                niquel1 = sorteio.Next(1,10);
+                lblNiquel1.Text = niquel1.ToString();
+            }
+
+            else if (contaNiquel == 2)
+            {
+                niquel2 = sorteio.Next(1, 10);
+                lblNiquel2.Text = niquel2.ToString();
+            }
+
+
+            else if (contaNiquel == 3)
+            {
+                niquel3 = sorteio.Next(1, 10);
+                lblNiquel3.Text = niquel3.ToString();
+            }
+
+            else
+            {
+                tmrNiquel.Enabled = false;
+                VerificacaoNiquel();
+                contaNiquel = 1;
+            }
+
+
+
+            if (contaGiro == 10)
+            {
+                contaNiquel++;
+                contaGiro = 0;
+            }
+
+            contaGiro++;
+        }
+
+        private void btnGirar_Click(object sender, EventArgs e)
+        {
+            //tmrSorteioGeral.Enabled = true;
+            MostraNiquel();
+            VerificacaoNiquel();
+        }
+        private void MostraNiquel()
+        {
+            niquel1 = sorteio.Next(1, 10);
+            niquel2 = sorteio.Next(1, 10);
+            niquel3 = sorteio.Next(1, 10);
+
+            lblNiquel1.Text = niquel1.ToString();
+            lblNiquel2.Text = niquel2.ToString();
+            lblNiquel3.Text = niquel3.ToString();   
+        }
+
+        private void VerificacaoNiquel()
+        {
+
+            if (niquel1 == niquel2 && niquel2 == niquel3)
+            {
+                MessageBox.Show("Parabens vc ganhou no jogo do tigrinho\n\n" +
+                    "Sequencia: " + niquel1.ToString() + " - " +
+                                    niquel2.ToString() + " - " +
+                                    niquel3.ToString(),
+                    "Tigrinho Wins !!!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                lblNiquel1.Text = String.Empty;
+                lblNiquel2.Text = String.Empty;
+                lblNiquel3.Text = String.Empty;
+                btnJogar.Text = "&Jogar";
+            }
+
+            else if (niquel1 != niquel2 && niquel2 != niquel3 && niquel1 != niquel3)
+            {
+                MessageBox.Show("que pessimo\n\n" +
+                    "Sequencia: " + niquel1.ToString() + " - " +
+                                    niquel2.ToString() + " - " +
+                                    niquel3.ToString(),
+                    "Tigrinho Wins !!!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                lblNiquel1.Text = String.Empty;
+                lblNiquel2.Text = String.Empty;
+                lblNiquel3.Text = String.Empty;
+                btnJogar.Text = "&Jogar";
+            }
+
+            else
+            {
+                MessageBox.Show("dois iguais parabens\n\n" +
+                    "Sequencia: " + niquel1.ToString() + " - " +
+                                    niquel2.ToString() + " - " +
+                                    niquel3.ToString(),
+                    "Tigrinho Wins !!!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                lblNiquel1.Text = String.Empty;
+                lblNiquel2.Text = String.Empty;
+                lblNiquel3.Text = String.Empty;
+                btnJogar.Text = "&Jogar";
+            }
+
+        }
+    }
+}
